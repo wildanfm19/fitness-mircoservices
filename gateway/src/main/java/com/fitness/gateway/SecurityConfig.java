30643bf6ae9c1f488a131d4fd77e3cc31dd38edc
wildanfm19 <dracko.maaruftm@gmail.com>
2025-10-07 13:47:38 +0700

    fix typo on gateway-service.yml and making use of Oauth2 in the SecurityConfig

--- gateway/src/main/java/com/fitness/gateway/SecurityConfig.java
+++ gateway/src/main/java/com/fitness/gateway/SecurityConfig.java
@@ -2,17 +2,26 @@ package com.fitness.gateway;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
+
     @Bean
-    public SecurityFilterChain springSecurityFilterChain(ServerHttpSecurity http){
-        return http.
-                csrf()
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http){
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeExchange(exchange -> exchange
+//                        .pathMatchers("/actuator/*").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
     }
 }
