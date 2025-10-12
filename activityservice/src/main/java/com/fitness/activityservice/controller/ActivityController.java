da600980b20af67ffaa92889dbc34bd66e10968f
wildanfm19 <dracko.maaruftm@gmail.com>
2025-10-12 22:05:04 +0700

    Added Fixed @RequestHeader in the controller for the error in the activityService for missing userId

--- activityservice/src/main/java/com/fitness/activityservice/controller/ActivityController.java
+++ activityservice/src/main/java/com/fitness/activityservice/controller/ActivityController.java
@@ -18,7 +18,10 @@ public class ActivityController {
     private final ActivityService activityService;
 
     @PostMapping
-    public ResponseEntity<ActivityResponse> trackActivity( @RequestBody ActivityRequest request){
+    public ResponseEntity<ActivityResponse> trackActivity( @RequestBody ActivityRequest request ,@RequestHeader("X-User-ID") String userId){
+        if(userId != null){
+            request.setUserId(userId);
+        }
         return ResponseEntity.ok(activityService.trackActivity(request));
     }
 
